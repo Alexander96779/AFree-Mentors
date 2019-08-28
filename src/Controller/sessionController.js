1d022cafa0e4d1f8f6b083d746ccfed6bf0b76c6
Alexander Prince <niyialexp@gmail.com>
2019-08-28 23:01:22 +0200

    adding and testing accept mentorship session feature

--- src/Controller/sessionController.js
+++ src/Controller/sessionController.js
@@ -23,6 +23,33 @@ class sessionController {
       error: 'Only users can create mentorShip session',
     });
   }
+
+  static acceptSession(req, res) {
+    if (req.user.userType === 'mentor') {
+      const { sessionId } = req.params;
+      // eslint-disable-next-line radix
+      const foundSession = sessionModal.find(s => s.sessionId === parseInt(sessionId));
+      if (req.user.id === foundSession.mentorId) {
+        const updatedSession = {
+          sessionId: foundSession.sessionId, mentorId: foundSession.mentorId, menteeId: foundSession.menteeId, questions: foundSession.questions, menteeEmail: foundSession.menteeEmail, status: 'accepted',
+        };
+        sessionModal[sessionModal.indexOf(foundSession)] = updatedSession;
+        return res.status(200).json({
+          status: 200,
+          message: 'session accepted',
+          data: updatedSession,
+        });
+      }
+      return res.status(401).json({
+        status: 401,
+        error: 'Unauthorized operation',
+      });
+    }
+    return res.status(403).json({
+      status: 403,
+      error: 'Unauthorized access',
+    });
+  }
 }
 
 // eslint-disable-next-line eol-last
